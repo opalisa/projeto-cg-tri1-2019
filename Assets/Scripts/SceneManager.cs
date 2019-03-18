59063ba942dcdf40548882137d9fd38137744fd3
LaylaChristina <laylachristinaferreiranaves@gmail.com>
2019-03-18 08:24:31 -0300

    Script SceneManager para mover o objeto Tape

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+   [SerializeField]
+   private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f);
+   
+   [SerializeField]
+   private Transform Tape = null;
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
     }
 }
